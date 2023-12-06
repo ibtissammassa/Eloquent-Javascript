ab0c00c60e635e2fafbe658cbdade98819e57648
ibtissammassa <bettysama16@gmail.com>
2023-12-06 23:48:44 +0100

    chapter 5

---
+++ Chapter 5/Your own loop/index.js
@@ -0,0 +1,13 @@
+
+function loop(value,test,update,body){
+    let n = value;
+    while(test(n)){
+        body(n);
+        n = update(n);
+    }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
